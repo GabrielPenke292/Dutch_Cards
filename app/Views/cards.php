a884edfc8e7fda8c167d5b31beaa288f3d451b6a
Gabriel Penke <gabri.penke@gmail.com>
2021-05-16 22:13:13 -0300

    layout tests

--- app/Views/cards.php
+++ app/Views/cards.php
@@ -3,17 +3,20 @@
 <?= $this->section('content') ?>
     <section id="cards">
         <div class="container w-50">
-            <div class="card justify-content-center">
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
+            <div class="row d-flex justify-content-center">
+                <div class="card bg-primary ">
+                    <div class="card bg-secondary" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
                     </div>
                 </div>
             </div>
+            
         </div>
     </section>
 <?= $this->endSection()?>
